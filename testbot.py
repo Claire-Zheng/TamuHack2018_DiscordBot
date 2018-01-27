fc20485de6294bdad1fd13d4b15bc274164d58a4
Claire-Zheng <claire.zheng.0110@gmail.com>
2018-01-27 16:14:57 -0600

    adding kms feature

--- testbot.py
+++ testbot.py
@@ -6,10 +6,15 @@ bot = commands.Bot(description="Short desc of your bot", command_prefix="!")
 
 @bot.command()
 async def ping():
-    await bot.say("Pong!") 
+    await bot.say("Pong!")
+
 @bot.command(pass_context=True)
 async def say(ctx, *, something):
 	await bot.say(something)
 	await bot.delete_message(ctx.message)
 
+@bot.command()
+async def kms():
+    await bot.add_reaction("Should I kill myself? One react and I'll do it.", :weary:)
+    
 bot.run('NDA2OTAzMTEzMzYzMDk1NTUy.DU54Yw.KV6VHd46qBTlIIgKMEmvvRQbfcw')
